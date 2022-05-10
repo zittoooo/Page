0c328c268b7f8872110eaeccf9e4c4a14bf8c957
jiholee <stellar7484@naver.com>
2022-05-10 20:50:09 +0900

    빌더 사용하기, Boolean과 boolean (primitive type은 null이 될 수 없음)

--- src/main/java/hello/mentoring/model/Member.java
+++ src/main/java/hello/mentoring/model/Member.java
@@ -1,11 +1,15 @@
 package hello.mentoring.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
+@Builder
+@AllArgsConstructor
 public class Member {
-    private boolean check;
+    private Boolean check;
     private Long id;
     private String memberName;
     private String address;
@@ -17,5 +21,13 @@ public class Member {
         this.check = false;
         this.memberName = memberName;
         this.address = address;
+    }
+
+    public Member(Long id, String name, String address, String upload, String store) {
+        this.check = false;
+        this.id = id;
+        this.memberName = name;
+        this.address = address;
+        this.attachFile = new UploadFile(upload, store);
     }
 }
--- src/main/java/hello/mentoring/model/MemberForm.java
+++ src/main/java/hello/mentoring/model/MemberForm.java
@@ -1,16 +1,18 @@
 package hello.mentoring.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
+@Builder
 public class MemberForm {
-    private boolean check;
+    private Boolean check;
 
     private Long id;
     private String memberName;
     private String address;
     private MultipartFile attachFile;
     private String fileName;
-
 }
