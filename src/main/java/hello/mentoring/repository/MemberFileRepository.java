6e6e9b236858d69e304bd5688c19970cf21a9839
jiholee <stellar7484@naver.com>
2022-05-15 21:01:49 +0900

    회원 정보 수정 - 새로운 객체 저장 후 지우기, 지울때는 모든 정보 확인 후 지움

--- src/main/java/hello/mentoring/repository/MemberFileRepository.java
+++ src/main/java/hello/mentoring/repository/MemberFileRepository.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 public interface MemberFileRepository {
     void saveOnFile(MemberDao memberDao) throws IOException;
     MemberDao findByNameOnFile(MemberDao memberDao);
-    void updateOnFile(MemberDao memberDao) throws IOException;
+    void updateOnFile(MemberDao findDao, MemberDao updateDao) throws IOException;
     void deleteOnFile(MemberDao memberDao) throws IOException;
+    void deleteOnFileByName(MemberDao memberDao) throws IOException;
+    void deleteOnFileById(MemberDao memberDao) throws IOException;
 }
